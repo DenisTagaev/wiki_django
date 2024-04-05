8401a0d2bd4246056d73618ca1dea92ad5dc4f43
Denis Tagaev <sivean.27@gmail.com>
2024-04-04 21:14:23 -0400

    Edit entry page introduced

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("wiki/search", views.index_search, name="wiki_search"),
     path("wiki/<str:entry>", views.entry, name="e_entry"),
     path("new_entry", views.new_entry, name="wiki_new_entry"),
+    path("wiki/<str:entry_title>/edit", views.edit_entry, name="wiki_edit_entry"),
     path("random_entry", views.random_entry, name="random_entry"),
     path("error", views.non_existent_entry, name="wiki_error"),
 ]
